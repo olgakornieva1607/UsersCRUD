7c96375eca99cb6e4e2b81782abe30d8ae1492b5
Olga <olga@Olgas-MacBook-Air.local>
2023-02-11 21:15:59 +0100

    created User class

---
+++ src/main/java/pl/coderslab/DbUtil.java
@@ -0,0 +1,35 @@
+package pl.coderslab;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.sql.SQLException;
+import java.sql.Connection;
+
+public class DbUtil {
+    private static DataSource dataSource;
+
+    public static Connection getConnection() throws SQLException {
+        return getInstance().getConnection();
+    }
+
+    private static DataSource getInstance() {
+        if (dataSource == null) {
+            try {
+                Context initContext = new InitialContext();
+                Context envContext = (Context) initContext.lookup("java:/comp/env");
+                dataSource = (DataSource) envContext.lookup("jdbc/users");
+            } catch (NamingException e) {
+                e.printStackTrace();
+            }
+        }
+        return dataSource;
+    }
+
+    public static Connection connect() throws SQLException {
+        Connection conn = DbUtil.getConnection();
+        return conn;
+    }
+
+}
